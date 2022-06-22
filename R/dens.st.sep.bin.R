7f076cd201eedc02757f310d6388e7912c23eb54
jagm03 <jonathan.gonzalez@kaust.edu.sa>
2022-06-22 15:16:56 +0200

    Separable estimator ready

---
+++ R/dens.st.sep.bin.R
@@ -0,0 +1,26 @@
+#' @importFrom spatstat.core densityAdaptiveKernel.ppp
+#' @importFrom spatstat.geom eval.im
+#' @export
+dens.st.sep.bin <- function(X, t,
+                            bw.xy = NULL, bw.t = NULL, #bandwidths
+                            ngroups.xy = NULL, ngroups.t = NULL, #groups
+                            dimt = 128, dimyx = 128, #resolution
+                            at = c("points", "bins") #at
+)
+{
+  at <- match.arg(at)
+  at.s <- switch (at, bins = "pixels", points = "points")
+  if (is.null(bw.xy)) {
+    h0 <- OS(X)
+    bw.xy <- bw.abram(X, h0)
+  }
+  Mtemporal <- dens.t.bin(ti = t, bw = bw.t, ngroups = ngroups.t,
+                          dimt = dimt, at = at)
+  Mspatial <- densityAdaptiveKernel.ppp(unmark(X), bw = bw.xy, at = at.s,
+                                        dimyx = dimyx, edge = T, ngroups = ngroups.xy)
+  Mst <- switch(at,
+                points = Mtemporal * Mspatial / length(t),
+                bins = lapply(Mtemporal$y, function(x) eval.im(Mspatial * x / length(t)))
+  )
+  return(Mst)
+}
