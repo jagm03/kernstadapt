5fb41f4f9364bf7f7ecd32258773b19cee8c5b79
jagm03 <jonathan.gonzalez@kaust.edu.sa>
2022-07-06 17:45:16 +0200

    work with marks included and optimised

--- R/separability.test.R
+++ R/separability.test.R
@@ -1,18 +1,24 @@
 #' @importFrom spatstat.geom verifyclass cut.ppp quadratcount.ppp
 #' @importFrom stats fisher.test
 #' @export
-separability.test <- function(X, t, nx = NULL, ny = NULL, nt = NULL, nperm = 1000)
+separability.test <- function(X, t = NULL, nx = NULL, ny = NULL, nt = NULL, nperm = 1000)
 {
   verifyclass(X, "ppp")
+  n <- npoints(X)
   if (missing(nx) || is.null(nx)) {
-    nx <- ceiling(npoints(X) ^ (1/6))
+    nx <- ceiling(n ^ (1/6))
   }
   if (missing(nx) || is.null(nt)) {
-    ny <- ceiling(npoints(X) ^ (1/6))
+    ny <- ceiling(n ^ (1/6))
   }
   if (missing(nt) || is.null(nt)) {
-    nt <- ceiling(npoints(X) ^ (1/6))
+    nt <- ceiling(n ^ (1/6))
   }
+  if(is.null(t)) t <- marks(X)
+  t <- checktt(t)
+  if(length(t) != n)
+    stop(paste("Length of temporal vector does not match number of spatial observations\n   npoints(X) = ",n,"; length(t) = ",length(t), sep = ""))
+
   X <- unmark(X)
   X <- setmarks(X, t)
   Tj <- split.ppp(cut(X, breaks = nt))
