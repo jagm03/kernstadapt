7f9767c2c872b1f8f02a2867724ddbc5b2a7ae07
jagm03 <jonathan.gonzalez@kaust.edu.sa>
2022-08-24 16:38:03 +0300

    conditioning to non-negative values

--- R/checking.R
+++ R/checking.R
@@ -19,5 +19,6 @@ checkt <- function(t){
   if(is.null(t)) stop("observation times must be supplied as 't' or as a numeric vector of marks to 'X'")
   if(!is.vector(t)) stop("'t' must be a vector")
   if(!is.numeric(t)) stop("'t' must be numeric")
+  if(sum(t < 0) != 0) stop("'t' must have non-negative components")
   return(t)
 }
