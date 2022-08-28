8a8bc334195fa1eac2c55726e982e8d26bae31c6
jagm03 <jonathan.gonzalez@kaust.edu.sa>
2022-08-28 15:38:38 +0300

    roxygen

--- R/separability.test.R
+++ R/separability.test.R
@@ -1,3 +1,43 @@
+#' Separability test for spatio-temporal point processes
+#'
+#' Performs a separability test of the first-order intensity function based on a Fisher Monte Carlo test of cell counts.
+#'
+#' @param X A spatial point pattern (an object of class \code{ppp}) with the spatial coordinates of the observations.
+#' @param t A numeric vector of temporal coordinates with equal length to the number of points in \code{X}. This gives the time associated with each spatial point.
+#' @param nx,ny,nt Numbers of quadrats in the \eqn{x,y} and \eqn{t} directions.
+#' @param nperm An integer specifying the number of replicates used in the Monte Carlo test.
+#'
+#' @details
+#' This function performs a basic test of the separability hypothesis in a manner similar to independence test in two-way contingency tables.
+#' The test is conditional on the observed number of points.
+#' It considers a regular division of the interval \eqn{T} into disjoint sub-intervals \eqn{T_1,...,T_{n_t}} and similarly a division of the window \eqn{W} into disjoint subsets \eqn{W_1, ..., W{n_x \times n_y}}.
+#' Then the function computes Fisher's test statistic and get a p-value based on Monte Carlos approximation.
+#'
+#' @return
+#' A list with class "htest" containing the following components:
+#' \item{p.value}{the approximate p-value of the test.}
+#' \item{method}{the character string "Separability test based on Fisher's for counting data".}
+#' \item{alternative}{a character string describing the alternative hypothesis.}
+#' \item{data.name}{a character string giving the name(s) of the data.}
+#'
+#' @references
+#' Ghorbani et al. (2021) Testing the first-order separability hypothesis for spatio-temporal point patterns, \emph{Computational Statistics & Data Analysis,} Vol. 161, p. 107245.
+#'
+#' @author Jonatan A. González
+#'
+#' @note This is a fast preliminary separability test.
+#'
+#' @examples
+#' \dontrun{
+#' data(aegiss)
+#' separability.test(X = unmark(aegiss), t = marks(aegiss))
+#' }
+#' \dontrun{
+#' data(amazon)
+#' separability.test(X = unmark(amazon), t = marks(amazon),
+#'                   nx = 10, ny = 10, nt = 20, nperm = 100)
+#'}
+#'
 #' @importFrom spatstat.geom verifyclass cut.ppp quadratcount.ppp
 #' @importFrom stats fisher.test
 #' @export
