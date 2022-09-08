bb5dd643d875241fc1ffea97a1bde257d3227447
jagm03 <jonathan.gonzalez@kaust.edu.sa>
2022-09-08 17:25:21 +0300

    Direct separable case done!!

--- R/dens.sep.direct.R
+++ R/dens.sep.direct.R
@@ -13,6 +13,7 @@ dens.sep.direct<- function(X, t = NULL,
     stop(paste("Length of temporal vector does not match number of spatial observations\n   npoints(X) = ",n,"; length(t) = ",length(t), sep = ""))
   at <- match.arg(at)
   at.s <- switch (at, bins = "pixels", points = "points")
+  range.t <- range(t)
   if (is.null(bw.xy)) {
     h0 <- OS(X)
     bw.xy <- bw.abram(X, h0)
@@ -25,19 +26,26 @@ dens.sep.direct<- function(X, t = NULL,
     if (length(bw.t) == 1)
       bw.t <- rep(bw.t, nT)
   }
-  PP <- split.ppp(X, f = factor(1:X$n))
-
-
-  Z <- mapply(density.default, x = Y, bw = bw.T, SIMPLIFY = F,
+  #Temporal part
+  PPt <- split(t, f = factor(1:X$n))
+  Z <- mapply(density.default, x = PPt, bw = bw.t, SIMPLIFY = F,
               MoreArgs = list(kernel = "gaussian", n = dimt,
                               from = range.t[1], to = range.t[2] ))
+  zx <- Z[[1]]$x
+  Edge0 <- sapply(bw.t, edge.t, dt = zx, TS = range.t[2], TI = range.t[1])
+  Ys <- sapply(Z, "[[", 'y') / Edge0
+  Ys <- apply(Ys, 1, sum)
+  Mtemporal <- switch(at,
+                      points = approxfun(x = zx, y = Ys)(t),
+                      bins = Ys)
 
-
+  #Spatial part
+  at.s <- switch (at, bins = "pixels", points = "points")
   Mspatial <- densityAdaptiveKernel.ppp(unmark(X), bw = bw.xy, at = at.s,
-                                        dimyx = dimyx, edge = T, ngroups = ngroups.xy)
+                                        dimyx = dimyx, edge = T, ngroups = Inf)
   Mst <- switch(at,
                 points = Mtemporal * Mspatial / length(t),
-                bins = lapply(Mtemporal$y, function(x) eval.im(Mspatial * x / length(t)))
+                bins = lapply(Mtemporal, function(x) eval.im(Mspatial * x / length(t)))
   )
   return(Mst)
 }
