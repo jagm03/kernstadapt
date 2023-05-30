e83ed02a427b042fa5d7c623d6434934c60e031a
jagm03 <jonathan.gonzalez@kaust.edu.sa>
2023-05-30 12:56:02 +0300

    making out the help

--- R/bw.abram.net.R
+++ R/bw.abram.net.R
@@ -10,34 +10,27 @@
 #' @param ... Aditional arguments passed to smoother to control the type of smoothing.
 #'
 #' @details
-#' This function returns a set of adaptive smoothing bandwidths driven by Abramson's (1982) method.
-#' The bandwidth at location \eqn{v} is given by
+#' This function returns a set of adaptive smoothing bandwidths driven by Abramson's (1982) method for a point pattern in a linear network.
+#' The bandwidth at location \eqn{u} is given by
 #' \deqn{
-#' \delta(v) = h0 * \mbox{min}\left[ \frac{1}{\gamma} \sqrt{\frac{n}{\lambda^{\mbox{t}}(v)}}, \mbox{\texttt{trim}} \right]
+#' \epsilon(u) = h0 * \mbox{min}\left[ \frac{1}{\gamma} \sqrt{\frac{n}{\tilde{lambda}(u)}}, \mbox{\texttt{trim}} \right]
 #' }{
 #' h0 * pmin((pilotvalues ^ (-0.5)) / gamma, trim)
 #' }
-#' where \eqn{\lambda^{\mbox{t}}(v)} is a pilot estimate of the temporally varying intensity and \eqn{\gamma} is a scaling constant depending on the pilot estimate.
+#' where \eqn{\tilde{\lambda}(u)} is a pilot estimate of the network varying intensity and \eqn{\gamma} is a scaling constant depending on the pilot estimate.
 #'
 #' @return
 #' If \code{at = "points"} (the default), the result is a numeric vector with one bandwidth for each data point in \code{X}.
-#' If \code{at = "bins"}, the output is an object with class "density" where \code{y} component is a vector with the estimated intensity values (see \link[stats]{density}).
+#' If \code{at = "pixels"}, the output is an object with class "linim"
 #'
 #' @references
 #'Abramson, I. (1982) On bandwidth variation in kernel estimates --- a square root law.
 #' \emph{Annals of Statistics}, \bold{10}(4), 1217-1223.\cr
 #'
-#' Davies, T.M. and Baddeley, A. (2018) Fast computation of spatially adaptive kernel estimates.
-#' \emph{Statistics and Computing}, \bold{28}(4), 937-956.\cr
-#'
 #' Davies, T.M., Marshall, J.C., and Hazelton, M.L. (2018)
 #' Tutorial on kernel estimation of continuous spatial and spatiotemporal relative risk.
 #' \emph{Statistics in Medicine}, \bold{37}(7), 1191-1221.\cr
 #'
-#' González J.A. and Moraga P. (2018)
-#' An adaptive kernel estimator for the intensity function of spatio-temporal point processes
-#' <https://arxiv.org/pdf/2208.12026.pdf>
-
 #' @author Jonatan A. González
 #'
 #' @examples
@@ -102,7 +95,7 @@ bw.abram.ppp <- function(X, h0,
            bw <- h0 * pmin((pilot.X^(-0.5))/gamma,trim)
          },
          pixels = {
-           bw <- eval.im(h0 * pmin((pilot^(-0.5))/gamma, trim))
+           bw <- eval.linim(h0 * pmin((pilot^(-0.5))/gamma, trim))
          })
 
   return(bw)
